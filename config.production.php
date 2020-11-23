04cd3d00958df4e6513e5d59f218e340d80379e7
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2020-11-23 12:27:16 +0100

    Update config.production.php

--- config.production.php
+++ config.production.php
@@ -5,6 +5,6 @@ return [
     'production' => true,
 
     // DocSearch credentials
-    'docsearchApiKey' => 'bfe2f70b574268a443312bd71480f20c',
-    'docsearchIndexName' => 'sculptor-devops',
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
 ];
