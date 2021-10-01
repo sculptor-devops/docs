cccfee87705963c08904834a81afe4d2a8d4c2f7
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2021-10-01 12:03:22 +0200

    Update menu-item.blade.php

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -2,7 +2,7 @@
     @if ($url = is_string($item) ? $item : $item->url)
         {{-- Menu item with URL--}}
         <a href="{{ $page->url($url) }}"
-            class="{{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-800' : '' }} nav-menu__item hover:text-blue-400"
+            class="{{ $level == 0 ? 'border p-2 mb-4 rounded-md bg-blue-300' : '' }} {{ 'lvl' . $level }} {{ $page->isActiveParent($item) ? 'lvl' . $level . '-active' : '' }} {{ $page->isActive($url) ? 'active font-semibold text-blue-800' : '' }} nav-menu__item hover:text-blue-400"
         >
             {{ $label }}
         </a>
